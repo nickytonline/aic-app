ef55f5f8b02fd32262825c333838b69978cd7a53
Nick Taylor <nick@iamdeveloper.com>
2023-05-22 23:07:55 -0400

    feat: search results render a list of artwork based on search query to AIC API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,17 @@ import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
 
 import './App.css';
+import { ArtResults } from './ArtResults';
 
 export function App() {
+	const [artworks, setArtworks] = useState();
+
 	function onSearchSubmit(query) {
-		// Search for the users's query.
-		// TODO: render the results, instead of logging them to the console.
-		// NOTE: `searchArtworks` currently returns local data, so that we
 		// don't make too many requests to the API! Once we've built out
 		// our UI, we need to make real requests!
 		// @see: ./src/api.js
-		searchArtworks(query).then((json) => {
-			console.log(json);
+		searchArtworks(query).then((searchResults) => {
+			setArtworks(searchResults?.data);
 		});
 	}
 
@@ -23,6 +23,7 @@ export function App() {
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
+			{artworks ? <ArtResults artworks={artworks} /> : null}
 			<Footer />
 		</div>
 	);
---
+++ src/components/ArtResults.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+export function ArtResults({ artworks }) {
+	return (
+		<ul className="art-results full-width">
+			{artworks.map((artwork) => {
+				const { image_id, title, artist_title } = artwork;
+				return (
+					<li key={image_id}>
+						<a href={`#${image_id}`}>{`${title} by ${artist_title}`}</a>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
+
+// The shape of the data we expect to receive based on the API response from the Art Institute of Chicago.
+ArtResults.propTypes = {
+	artworks: PropTypes.arrayOf(
+		PropTypes.shape({
+			_score: PropTypes.number.isRequired,
+			thumbnail: PropTypes.shape({
+				alt_text: PropTypes.string.isRequired,
+				width: PropTypes.number.isRequired,
+				height: PropTypes.number.isRequired,
+			}).isRequired,
+			date_display: PropTypes.string.isRequired,
+			artist_title: PropTypes.string.isRequired,
+			image_id: PropTypes.string.isRequired,
+			title: PropTypes.string.isRequired,
+		})
+	),
+};
